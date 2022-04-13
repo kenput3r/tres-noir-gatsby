7e234545c753b4d51a5428910df3f5c79047ba7d
j-Riv <jose.alfredo.rivera@gmail.com>
2022-04-13 15:16:38 -0700

    Pinned Gatsby to 4.7.2, testing rebuld

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
   siteMetadata: {
     title: `Tres Noir`,
     description: `Tres Noir is an independent eyewear company located in Santa Ana, Calif.`,
-    author: `@kenput3r`,
+    author: `@SuavecitoInc`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -25,7 +25,15 @@ module.exports = {
       },
     },
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        defaults: {
+          formats: [`webp`, `auto`],
+          quality: 80,
+        },
+      },
+    },
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-plugin-manifest`,
