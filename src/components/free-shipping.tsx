91ba9f344c5c57fcd779c89399a56d030d1d2727
Jonathan Picazo <jonathanpicazo@outlook.com>
2022-05-23 13:58:06 -0700

    finished adding new hero and about banners

--- src/components/free-shipping.tsx
+++ src/components/free-shipping.tsx
@@ -10,12 +10,18 @@ const Component = styled.div`
     margin-top: 0.5rem;
     margin-bottom: 0.2rem;
     font-size: 1.5rem;
+    @media screen and (max-width: 480px) {
+      font-size: 1.3rem;
+    }
   }
   .bottom-msg {
     color: var(--color-grey-dark);
     font-family: var(--sub-heading-font);
     font-weight: normal;
     font-size: 1.25rem;
+    @media screen and (max-width: 480px) {
+      font-size: 1rem;
+    }
   }
 `
 
