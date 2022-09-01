2b18534c86192585b0c66c0bc3333ba606815476
José Alfredo Rivera Turcios <jose.alfredo.rivera@gmail.com>
2022-09-01 11:28:36 -0700

    Update newsletter.ts

--- src/api/newsletter.ts
+++ src/api/newsletter.ts
@@ -1,9 +1,11 @@
 import fetch from "node-fetch"
 
 export default async function klaviyoFormHandler(req, res) {
+  console.log('TRY POSTING TO KLAVIYO')
   try {
     const reqEmail = req.body.inEmail
     const url: string = `https://a.klaviyo.com/api/v2/list/R4y2R5/subscribe?api_key=${process.env.KLAVIYO_PRIVATE_KEY}`
+    console.log('URL', url)
     const options = {
       method: "POST",
       headers: {
