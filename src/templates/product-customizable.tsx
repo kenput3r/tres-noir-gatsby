dc3b185a4d6dfa2e967658e8ffa04a462023f310
Jonathan Picazo <jonathanpicazo@outlook.com>
2023-08-15 14:43:25 -0700

    added logic to swap polarized image if lens color is different than original, non-polarized version

--- src/templates/product-customizable.tsx
+++ src/templates/product-customizable.tsx
@@ -181,6 +181,7 @@ const Page = styled.div`
       max-width: 100% !important;
       text-decoration: none;
       text-align: center;
+      appearance: initial;
       -webkit-appearance: button-bevel;
       border-radius: 0%;
     }
@@ -345,6 +346,14 @@ const ProductCustomizable = ({
 
   const [showPolarizedModal, setShowPolarizedModal] = useState<boolean>(false)
 
+  // used to swap polarized image if the lens color is different from original sku
+  const [polarizedImage, setPolarizedImage] = useState<
+    {
+      data: any
+      title: string
+    }[]
+  >([])
+
   // return default Product Page if contentful values do not exist
   const quantityLevels = useQuantityQuery(
     shopifyProduct.handle,
@@ -361,6 +370,45 @@ const ProductCustomizable = ({
     // if switch is toggled
     if (evt.target.checked) {
       if (polarizedVariant) {
+        // check if the lens color for polarized is different than unpolarized
+        // if it is, use string matching/interpolation to get the correct image
+        const defaultLensColor: string = selectedVariant.shopify.title
+          .toLowerCase()
+          .split("-")[1]
+          .replace("lens", "")
+          .trim()
+        const polarizedLensColor: string = polarizedVariant.shopify.title
+          .toLowerCase()
+          .split("-")[1]
+          .replace("lens", "")
+          .replace("polarized", "")
+          .trim()
+        // checks if polarized lens color is different from original lens color for a variant
+        if (polarizedLensColor !== defaultLensColor) {
+          // prefixes used for contentful image key
+          let imagePrefix = "sunGlasses"
+          const imageSuffix = "Lenses"
+          // if the polarized image is gradient, change prefix to use gradient contentful image keys
+          if (polarizedLensColor.includes("gradient")) {
+            imagePrefix = "gradientTint"
+          }
+          // format color for contentful image key
+          let formattedLensColor = polarizedLensColor.replace("gradient", "")
+          // capitalize color
+          formattedLensColor =
+            formattedLensColor.charAt(0).toUpperCase() +
+            formattedLensColor.slice(1)
+          // join prefix and suffix to get contentful image key
+          const polarizedImageKey =
+            imagePrefix + formattedLensColor + imageSuffix
+
+          const polarizedImageValue =
+            selectedVariant.contentful.customizations[polarizedImageKey]
+          // only swap the image if the key is valid, this prevents loading bad images
+          if (polarizedImageValue) {
+            setPolarizedImage([polarizedImageValue])
+          }
+        }
         // disable customize
         customizeBtn?.classList.add("disable")
         // set polarized variant to non polarized version
@@ -391,10 +439,12 @@ const ProductCustomizable = ({
           shopify: polarizedVariant.shopify,
         })
       }
+      // polarized switch has been set to off, reinitialize state and show old images
+      setPolarizedImage([])
     }
   }
 
-  // sets polarizedVariant to the correct polarized variant for corresponding frame
+  // useEffect for initializing polarizedVariant on selectedVariant change
   useEffect(() => {
     if (lensType === LensType.GLASSES) return
     const contentfulData = selectedVariant.contentful
@@ -556,6 +606,9 @@ const ProductCustomizable = ({
       (_variant: any) => _variant.sku === variant.sku
     )
     if (shopify) {
+      // clear polarized image on change
+      setPolarizedImage([])
+      //
       setSelectedVariant({
         contentful: variant,
         shopify,
@@ -601,7 +654,10 @@ const ProductCustomizable = ({
             ],
           },
         ],
-        selectedVariant.contentful.imageSet[0].data,
+        // sets cart image to be correct polarized image if its a mismatch from non-polarized variant
+        polarizedImage.length !== 0
+          ? polarizedImage[0].data
+          : selectedVariant.contentful.imageSet[0].data,
         matchingKey
       )
 
@@ -679,9 +735,11 @@ const ProductCustomizable = ({
           <div className="col images">
             <ProductCarousel
               imageSet={
-                selectedVariant?.contentful &&
-                lensType === LensType.GLASSES &&
-                selectedVariant.contentful.imageSetClear
+                polarizedImage.length !== 0
+                  ? polarizedImage
+                  : selectedVariant?.contentful &&
+                    lensType === LensType.GLASSES &&
+                    selectedVariant.contentful.imageSetClear
                   ? selectedVariant.contentful.imageSetClear
                   : selectedVariant.contentful.imageSet
               }
@@ -877,6 +935,184 @@ export const query = graphql`
         imageSetClear {
           data: gatsbyImageData(layout: CONSTRAINED, width: 2048, height: 1365)
           title
+        }
+        customizations {
+          bifocal {
+            data: gatsbyImageData(
+              placeholder: TRACED_SVG
+              quality: 60
+              width: 800
+            )
+            title
+          }
+          bifocalGradientTintSmokeLenses {
+            data: gatsbyImageData(
+              placeholder: TRACED_SVG
+              quality: 60
+              width: 800
+            )
+            title
+          }
+          bifocalGradientTintBrownLenses {
+            data: gatsbyImageData(
+              placeholder: TRACED_SVG
+              quality: 60
+              width: 800
+            )
+            title
+          }
+          bifocalGradientTintG15Lenses {
+            data: gatsbyImageData(
+              placeholder: TRACED_SVG
+              quality: 60
+              width: 800
+            )
+            title
+          }
+          clear {
+            data: gatsbyImageData(
+              placeholder: TRACED_SVG
+              quality: 60
+              width: 800
+            )
+            title
+          }
+          gradientTintSmokeLenses {
+            data: gatsbyImageData(
+              placeholder: TRACED_SVG
+              quality: 60
+              width: 800
+            )
+            title
+          }
+          gradientTintBrownLenses {
+            data: gatsbyImageData(
+              placeholder: TRACED_SVG
+              quality: 60
+              width: 800
+            )
+            title
+          }
+          gradientTintG15Lenses {
+            data: gatsbyImageData(
+              placeholder: TRACED_SVG
+              quality: 60
+              width: 800
+            )
+            title
+          }
+          sunGlassesSmokeLenses {
+            data: gatsbyImageData(
+              placeholder: TRACED_SVG
+              quality: 60
+              width: 800
+            )
+            title
+          }
+          sunGlassesBrownLenses {
+            data: gatsbyImageData(
+              placeholder: TRACED_SVG
+              quality: 60
+              width: 800
+            )
+            title
+          }
+          sunGlassesGreenLenses {
+            data: gatsbyImageData(
+              placeholder: TRACED_SVG
+              quality: 60
+              width: 800
+            )
+            title
+          }
+          sunGlassesOrangeLenses {
+            data: gatsbyImageData(
+              placeholder: TRACED_SVG
+              quality: 60
+              width: 800
+            )
+            title
+          }
+          sunGlassesYellowLenses {
+            data: gatsbyImageData(
+              placeholder: TRACED_SVG
+              quality: 60
+              width: 800
+            )
+            title
+          }
+          sunGlassesBlueLenses {
+            data: gatsbyImageData(
+              placeholder: TRACED_SVG
+              quality: 60
+              width: 800
+            )
+            title
+          }
+          sunGlassesG15Lenses {
+            data: gatsbyImageData(
+              placeholder: TRACED_SVG
+              quality: 60
+              width: 800
+            )
+            title
+          }
+          sunGlassesSmokeLensesBifocal {
+            data: gatsbyImageData(
+              placeholder: TRACED_SVG
+              quality: 60
+              width: 800
+            )
+            title
+          }
+          sunGlassesBrownLensesBifocal {
+            data: gatsbyImageData(
+              placeholder: TRACED_SVG
+              quality: 60
+              width: 800
+            )
+            title
+          }
+          sunGlassesGreenLensesBifocal {
+            data: gatsbyImageData(
+              placeholder: TRACED_SVG
+              quality: 60
+              width: 800
+            )
+            title
+          }
+          sunGlassesOrangeLensesBifocal {
+            data: gatsbyImageData(
+              placeholder: TRACED_SVG
+              quality: 60
+              width: 800
+            )
+            title
+          }
+          sunGlassesYellowLensesBifocal {
+            data: gatsbyImageData(
+              placeholder: TRACED_SVG
+              quality: 60
+              width: 800
+            )
+            title
+          }
+          sunGlassesBlueLensesBifocal {
+            data: gatsbyImageData(
+              placeholder: TRACED_SVG
+              quality: 60
+              width: 800
+            )
+            title
+          }
+          sunGlassesG15LensesBifocal {
+            data: gatsbyImageData(
+              placeholder: TRACED_SVG
+              quality: 60
+              width: 800
+            )
+            title
+          }
         }
         id
         lensColor
