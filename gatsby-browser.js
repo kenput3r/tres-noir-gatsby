1affa8ad64eb4d31919338ac84c2dbf71b34283a
Jonathan Picazo <jonathanpicazo@outlook.com>
2024-08-06 15:02:57 -0700

    feat: add useDiscountIdentifier hook for rendering discounted pricing based on contentful setting, chore: remove CustomerContext and /auth routes since its not being used

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,6 @@
 import React from "react"
 import { CustomizeProvider } from "./src/contexts/customize"
 import { CartProvider } from "./src/contexts/cart"
-import { CustomerProvider } from "./src/contexts/customer"
 import { RxInfoContextProvider } from "./src/contexts/rxInfo"
 import { ErrorModalProvider } from "./src/contexts/error"
 import ErrorBoundary from "./src/components/error-boundary"
@@ -9,15 +8,13 @@ import ErrorBoundary from "./src/components/error-boundary"
 export const wrapRootElement = ({ element }) => {
   return (
     <ErrorModalProvider>
-      <CustomerProvider>
-        <CartProvider>
-          <CustomizeProvider>
-            <RxInfoContextProvider>
-              <ErrorBoundary>{element}</ErrorBoundary>
-            </RxInfoContextProvider>
-          </CustomizeProvider>
-        </CartProvider>
-      </CustomerProvider>
+      <CartProvider>
+        <CustomizeProvider>
+          <RxInfoContextProvider>
+            <ErrorBoundary>{element}</ErrorBoundary>
+          </RxInfoContextProvider>
+        </CustomizeProvider>
+      </CartProvider>
     </ErrorModalProvider>
   )
 }
