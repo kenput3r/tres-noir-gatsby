f28026693e9e18cd021cea1393616e9023aa305a
j-Riv <jose.alfredo.rivera@gmail.com>
2024-12-04 20:21:09 -0800

    refactor: Updated Shopify helper functions and added formatPrice as pricing is now a number and zeros are dropped

--- src/helpers/shopify.ts
+++ src/helpers/shopify.ts
@@ -1,8 +1,25 @@
 // determines whether a product is discounted for on sale
-export function isDiscounted(price: string, compareAtPrice: string) {
-  if (Number(price) === 0 || Number(compareAtPrice) === 0) return false
-  if (Number(compareAtPrice) > Number(price)) {
+// export function isDiscounted(price: string, compareAtPrice: string) {
+//   if (Number(price) === 0 || Number(compareAtPrice) === 0) return false
+//   if (Number(compareAtPrice) > Number(price)) {
+//     return true
+//   }
+//   return false
+// }
+
+export function isDiscounted(
+  price: string | number,
+  compareAtPrice: string | number
+) {
+  price = Number(price)
+  compareAtPrice = Number(compareAtPrice)
+  if (price === 0 || compareAtPrice === 0) return false
+  if (compareAtPrice > price) {
     return true
   }
   return false
+}
+
+export function formatPrice(price: string) {
+  return Number(price).toFixed(2)
 }
