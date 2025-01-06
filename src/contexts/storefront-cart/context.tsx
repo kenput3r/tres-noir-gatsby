6f28bc44935e3ac569d690bc1756fbc38cf2f080
j-Riv <jose.alfredo.rivera@gmail.com>
2025-01-06 10:48:13 -0800

    refactor: Fixed with new cart type

--- src/contexts/storefront-cart/context.tsx
+++ src/contexts/storefront-cart/context.tsx
@@ -1,65 +1,8 @@
 import { IGatsbyImageData } from "gatsby-plugin-image"
 import { createContext } from "react"
-import { CustomLineItem } from "../../types/checkout"
 import { SelectedVariants } from "../../types/global"
 
-import type { Cart } from "./types/storefront-cart"
-
-type CartContextType = {
-  isDrawerOpen: boolean
-  setIsDrawerOpen: (value: boolean) => void
-  isCartDrawerOpen: boolean
-  setIsCartDrawerOpen: (value: boolean) => void
-  isActive: string
-  setIsActive: (value: string) => void
-  closeDrawer: () => void
-  cart: Cart | undefined
-  isAddingToCart: boolean
-  setIsAddingToCart: (value: boolean) => void
-  addProductToCart: (
-    variantId: string,
-    quantity: number,
-    sku: string,
-    image: IGatsbyImageData,
-    shouldOpenDrawer?: boolean
-  ) => void
-  addProductsToCart: (
-    lineItems: { variantId: string; quantity: number }[]
-  ) => void
-  addSunglassesToCart: (
-    lineItems: CustomLineItem[],
-    image: IGatsbyImageData,
-    key: string
-  ) => void
-  addProductCustomToCart: (
-    items: CustomLineItem[],
-    key: string,
-    image: IGatsbyImageData,
-    resumeData: SelectedVariants,
-    sku: string,
-    handle: string,
-    activateDrawer: boolean
-  ) => void
-  removeProductFromCart: (lineItemId: string, imageId: string) => void
-  removeProductsFromCart: (
-    lineItemIds: string[],
-    imageId: string,
-    hasDiscount?: boolean
-  ) => void
-  removeCustomProductWithId: (id: string) => void
-  updateProductInCart: (
-    lineId: string,
-    variantId: string,
-    quantity: number,
-    imageId: string
-  ) => void
-  addDiscountCode: (code: string) => void
-  removeDiscountCode: () => void
-  isRemovingFromCart: boolean
-  getAppliedDiscountCode: () => string
-  updateShipInsureAttribute: (enableShipInsure: boolean) => void
-  isShipInsureEnabled: boolean
-}
+import type { CartContextType, CustomLineItem } from "./types/storefront-cart"
 
 const defaultContext: CartContextType = {
   isDrawerOpen: false,
