a649c0376e26cc4ec7397d3764755f9a1b54bf9a
j-Riv <jose.alfredo.rivera@gmail.com>
2025-01-13 10:29:32 -0800

    chore: Reverting after test

--- src/hooks/useDiscountIdentifier.tsx
+++ src/hooks/useDiscountIdentifier.tsx
@@ -10,16 +10,11 @@ export const useDiscountIdentifier = () => {
     }
   `)
   return {
-    discountIdentifier: "20% OFF RX and Custom Lenses",
-    enableDiscountIdentifier: true,
+    discountIdentifier: data.contentfulHomepage.discountIdentifier as string,
+    enableDiscountIdentifier: data.contentfulHomepage
+      .enableDiscountIdentifier as boolean,
     overwriteLabel: true,
   }
-  // return {
-  //   discountIdentifier: data.contentfulHomepage.discountIdentifier as string,
-  //   enableDiscountIdentifier: data.contentfulHomepage
-  //     .enableDiscountIdentifier as boolean,
-  //   overwriteLabel: true,
-  // }
 }
 
 export default useDiscountIdentifier
